bf48932cee94110aed44dafdd85ab78a223797c9
socket772 <paolotto.ghilardi@gmail.com>
2021-06-14 11:25:46 +0200

    Pulizia file vecchi e non utilizzati
    Rimossa la pagina per le "Richieste" e per la gestione degli uffici, rimossa la WBS e file word contenente la scaletta

--- Gestore/Uffici/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <h1>Genera l'ordine</h1>
-        <br><br>
-        <button onclick="window.location.href='./form.php';">Modifica invetario</button>
-        <br><br>
-        <button onclick="window.location.href='./visualizzaUffici.php';">Visualizza invetario</button>
-    </body>
-</html>
